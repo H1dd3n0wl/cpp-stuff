047b5dbc738c81fb64ad215b98eb5a9f539ddd7a
H1dd3n0wl <sviter1232@gmail.com>
2023-09-15 22:42:07 +0300

    redesined the eratosthenes sieve

--- algorithms/math/eratosthenes/README.md
+++ algorithms/math/eratosthenes/README.md
@@ -1,3 +1,9 @@
 ## Algorithm description
 
-The eratosthenes sieve helps us to find all prime numbers up to any given limit. In this version of algorithm we have all of maximum prime divisor for all numbers from 1 to n. So you can factorise the number for $O(d(n))$, where $d(n)$ is count of prime divisors of n and check for prime for $O(1)$. The summary time of building the sieve is $O(n loglogn)$.
+The classic eratosthenes sieve helps us to find all prime numbers up to any given limit. 
+
+There we have some versions of algorithm. Classic version uses less memory than linear-time version, but it can only give us answer about primality of numbers and it's time completion is $O(nloglogn)$. 
+
+With the help of linear-time version you can factorise the number for $O(d(n))$, where $d(n)$ is count of prime divisors of n and check for prime for $O(1)$.
+
+The block version of eratosthenes sieve give us possibility to use prime numbers in about classical time but with $O(\sqrt n)$ memory complexity.
--- algorithms/math/eratosthenes/eratosthenes.hpp
+++ algorithms/math/eratosthenes/eratosthenes.hpp
@@ -1,27 +1,49 @@
+#include <array>
+#include <cmath>
 #include <vector>
 
+namespace classic_erat {
 const int N = 1e7;
 
-std::vector<int> sieve(N);
+std::vector<bool> prime(N + 1, true);
 
 void build() {
-    for (int i = 0; i < N; ++i) {
-        sieve[i] = i;
-    }
+    prime[0] = prime[1] = false;
 
     for (int i = 2; i * i <= N; ++i) {
-        if (sieve[i] == i) {
-            for (int k = i * i; k < N; k += i) {
-                sieve[k] = i;
+        if (prime[i]) {
+            for (int k = i * i; k <= N; k += i) {
+                prime[k] = false;
             }
         }
     }
 }
 
+bool isPrime(int p) { return prime[p]; }
 
-inline bool isPrime(int p) {
-    return sieve[p] == p;
+}  // namespace classic_erat
+
+namespace linear_erat {
+
+const int N = 1e7;
+
+std::array<int, N + 1> sieve;
+std::array<int, N> pr;
+int cnt = 0;
+
+void build() {
+    for (int i = 2; i <= N; ++i) {
+        if (sieve[i] == 0) {
+            sieve[i] = i;
+            pr[cnt++] = i;
+        }
+        for (int j = 0; j < cnt && pr[j] <= sieve[i] && i * pr[j] <= N; ++j) {
+            sieve[i * pr[j]] = pr[j];
+        }
+    }
 }
+
+inline bool isPrime(int p) { return sieve[p] == p; }
 
 std::vector<int> factorize(int n) {
     std::vector<int> res;
@@ -31,3 +53,48 @@ std::vector<int> factorize(int n) {
     }
     return res;
 }
+
+}  // namespace linear_erat
+
+namespace block_erat {
+
+const int SQRT_N = 1e5;  // sqrt of input size
+const int S = 1e4;       // size of block
+
+std::array<bool, SQRT_N> nprime;
+std::array<bool, S> bl;
+std::array<int, SQRT_N> prime;
+int cnt;
+
+void build(int n) {
+    int nsqrt = static_cast<int>(sqrtl(n));
+
+    for (int i = 2; i <= nsqrt; ++i) {
+        if (!nprime[i]) {
+            prime[cnt++] = i;
+            for (int j = i * i; j <= nsqrt; j += i) {
+                nprime[j] = true;
+            }
+        }
+    }
+
+    for (int k = 0; k <= n / S; ++k) {
+        bl.fill(false);
+        int start = k * S;
+        for (int i = 0; i < cnt; ++i) {
+            int start_idx = (start + prime[i] - 1) / prime[i];
+            int j = std::max(start_idx, 2) * prime[i] - start;
+            for (; j < S; j += prime[i]) {
+                bl[j] = true;
+            }
+        }
+        if (k == 0) {
+            bl[0] = bl[1] = true;
+        }
+        for (int i = 0; i < S && start + i <= n; ++i) {
+            /*if bl[i] is false then start + i is prime*/
+        }
+    }
+}
+
+}  // namespace block_erat
