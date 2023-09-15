14ccc0e55604743d5769e018237ed7d57c303f10
H1dd3n0wl <sviter1232@gmail.com>
2023-09-15 22:53:21 +0300

    redesigned fenwick tree

--- algorithms/ds/fenwick/README.md
+++ algorithms/ds/fenwick/README.md
@@ -1,3 +1,3 @@
 ## Algorithm description
 
-A Fenwick tree is a data structure that can efficiently update values and calculate prefix sums in an array of values. Building is held for $O(nlogn)$ time, updating and sum calculating is for $O(logn)$
+A Fenwick tree is a data structure that can efficiently update values (but only one per query) and calculate prefix sums in an array of values. Building is held for $O(nlogn)$ time, updating and sum calculating is for $O(logn)$.
--- algorithms/ds/fenwick/fenwick.hpp
+++ algorithms/ds/fenwick/fenwick.hpp
@@ -1,29 +1,34 @@
-#include <vector>
+#include <array>
 
+template <std::size_t N>
 class FenwickTree {
 private:
-    int n;
-    std::vector<long long> t;
+    std::array<long long, N> t_;
 
 public:
-
     inline void add(int v, long long delta) {
-        for (; v < n; v |= v + 1) {
-            t[v] += delta;
+        for (; v < N; v |= v + 1) {
+            t_[v] += delta;
         }
     }
 
     inline long long sum(int v) {
         long long res = 0;
-        for (v; v >= 0; v &= v+1, v--) {
-            res += t[v];
+        for (v; v >= 0; v &= v + 1, v--) {
+            res += t_[v];
         }
         return res;
     }
 
-    inline long long sum(int l, int r) {
-        return sum(r) - sum(l - 1);
+    inline long long sum(int l, int r) { return sum(r) - sum(l - 1); }
+
+    FenwickTree(std::array<long long, N> t) {
+        for (std::size_t i = 0; i < N; ++i) {
+            add(i, t[i]);
+        }
     }
 
-    FenwickTree(int n) : n{n}, t(n, 0) {}
+    FenwickTree() {
+        t_.fill(0);
+    }
 };
