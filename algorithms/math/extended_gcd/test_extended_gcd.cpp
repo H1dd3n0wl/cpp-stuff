ac75980d7206878b7b1f3118266cafdc492b646a
H1dd3n0wl <sviter1232@gmail.com>
2023-09-18 19:55:28 +0300

    extended gcd and diofantine equation added

---
+++ algorithms/math/extended_gcd/test_extended_gcd.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include "extended_gcd.hpp"
+
+TEST(extended_gcd, math) {
+	ASSERT_EQ(1, 1);
+}
+
+int main(int argc, char **argv) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
